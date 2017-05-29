d8b95b3bf6b774e7ebe2f1b3051d019be881a493
KamuelaFranco <kamuela.franco@gmail.com>
2017-05-30 00:14:55 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const fetch = require('isomorphic-fetch');
+const play = require('play');
+
+if (process.argv.length < 3) {
+  console.log('USAGE: node deliveroo.js <postcode>');
+  process.exit();
+}
+
+const postcode = process.argv[2];
+const endpoint = `https://deliveroo.co.uk/restaurants/nottingham/the-meadows?postcode=${postcode}`;
+const timeoutDuration = 30000;
+const normalNumberOfAsapRestaurants = 3;
+
+const fetchFunction = (currentTimeout = null) => {
+  fetch(endpoint, {
+    headers: {
+      Accept: 'application/json',
+    } })
+        .then(result => result.json())
+        .then(({ restaurants }) => {
+          const availableRestaurants = restaurants.filter(({ asap }) => asap);
+          if (availableRestaurants.length < normalNumberOfAsapRestaurants) {
+            console.log('Deliveroo is not available at the moment');
+            console.log(`Checking again in ${timeoutDuration}msec`);
+            const timeout = setTimeout(() => fetchFunction(timeout), timeoutDuration);
+          } else {
+            clearTimeout(currentTimeout);
+            console.log('Deliveroo is available');
+            play.sound('./node_modules/wavs/sfx/alarm.wav');
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+          clearTimeout(currentTimeout);
+        });
+};
+
+fetchFunction();
